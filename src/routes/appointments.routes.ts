668566d46c13f49b6aaa916ff86c3f6d0c93b07e
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2020-04-18 19:04:54 -0300

    Create repository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,32 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO } from 'date-fns';
 
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-
   const parsedDate = startOfHour(parseISO(date));
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });

1c04bbe8f7412f69ecfee96c13b8d82daa59c1d1
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2020-04-19 00:23:41 -0300

    Validate user credentials

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import AuthenticaUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticaUserService = new AuthenticaUserService();
+
+    const { user } = await authenticaUserService.execute({ email, password });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;

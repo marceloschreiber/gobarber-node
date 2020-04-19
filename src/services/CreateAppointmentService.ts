40fdff1cc25d84d746b4e882d24dc88eb898a406
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2020-04-19 00:07:33 -0300

    Persist user to database

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentRepository = getCustomRepository(AppointmentRepository);
 
     const appointmentDate = startOfHour(date);
@@ -20,12 +20,12 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked');
+      throw new Error('This appointment is already booked');
     }
 
     const appointment = appointmentRepository.create({
-      provider,
-      date,
+      provider_id,
+      date: appointmentDate,
     });
 
     await appointmentRepository.save(appointment);
